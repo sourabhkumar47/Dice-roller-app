63d2f2e80d54ea38bebc8d528e32b6e5fef9af39
Sourabh kumar <sourabh.47512@gmail.com>
2021-12-25 23:01:43 +0530

    Changes textView to imageView (Added image of dices ) and added comments

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 /*
 * This activity allow the user to roll a dice and view the result
@@ -15,9 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var rollButton: Button = findViewById(R.id.button)
+        //Find Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
 
+        //it will set the click listener on the button when the user tap the button
         rollButton.setOnClickListener { rollDice() }
+
+        //Do a roll dice when app start
+        rollDice()
     }
 
 /*
@@ -27,16 +31,31 @@ class MainActivity : AppCompatActivity() {
         // Create  new dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        
+        //Find the image in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView1)
 
-        // Update the screen with the roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        //Determine the Resourse id of dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    //Update the Imageview
+    diceImage.setImageResource(drawableResource)
+
+    //Updating the content Description
+    diceImage.contentDescription = diceRoll.toString()
 
     }
 }
-
+//Dice with fixed no of sides
 class Dice(val numSides: Int) {
 
+    //Random dice roll
     fun roll(): Int {
         return (1..numSides).random()
     }
